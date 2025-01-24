7705ba70633c8669fbfd4e5e4f0c1c473884ba26
Mayankjatm35 <jatmayankjatm35@gmail.com>
2025-01-24 12:19:17 +0530

    Update __main__.py

--- devgagan/__main__.py
+++ devgagan/__main__.py
@@ -2,10 +2,10 @@
 # File Name: __main__.py
 # Description: A Pyrogram bot for downloading files from Telegram channels or groups 
 #              and uploading them back to Telegram.
-# Author: Gagan
-# GitHub: https://github.com/devgaganin/
-# Telegram: https://t.me/team_spy_pro
-# YouTube: https://youtube.com/@dev_gagan
+# Author: Stranger
+# Owner: https://t.me/oo7jatji
+# Telegram: https://t.me/WarriorUnitsBots
+# YouTube: https://youtube.com/@WarriorUnitsAnime
 # Created: 2025-01-11
 # Last Modified: 2025-01-11
 # Version: 2.0.5
@@ -40,10 +40,10 @@ async def devggn_boot():
 📂 Bot Deployed successfully ...
 📝 Description: A Pyrogram bot for downloading files from Telegram channels or groups 
                 and uploading them back to Telegram.
-👨‍💻 Author: Gagan
-🌐 GitHub: https://github.com/devgaganin/
-📬 Telegram: https://t.me/team_spy_pro
-▶️ YouTube: https://youtube.com/@dev_gagan
+👨‍💻 Author: Stranger
+🌐 GitHub: https://t.me/WarriorUnitsBots
+📬 Telegram: https://t.me/WarriorUnitsBots
+▶️ YouTube: https://youtube.com/@WarriorUnitsAnime
 🗓️ Created: 2025-01-11
 🔄 Last Modified: 2025-01-11
 🛠️ Version: 2.0.5
